1ac99fe9e741e2a247008289f83a1ac8ca5ca81f
hadi chebbo <chebbohadi5@gmail.com>
2026-05-11 22:14:38 +0300

    fixed namespaces and import after path modification + get test question api implementation

---
+++ backend/app/Http/Controllers/Api/V1/Student/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Student;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Student\QuestionResource;
+use App\Models\Question;
+use App\Traits\ApiResponseTrait;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    use ApiResponseTrait;
+
+    public function getQuestions()
+    {
+        $questions = Question::with('options')->get();
+        
+        return $this->success(QuestionResource::collection($questions),"Test Questions Fetched Successfully",200);
+    }
+}
---
+++ backend/routes/api/student/quiz.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\Student\TestController;
+
+Route::prefix('test')
+    ->controller(TestController::class)
+    ->middleware(['auth:sanctum'])
+    ->group(function (): void {
+        Route::get('/questions','getQuestions');
+    });
