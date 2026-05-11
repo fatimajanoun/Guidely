49ff4a9b8aee4e93d4c48d1e81b45e35928a442e
hadi chebbo <chebbohadi5@gmail.com>
2026-05-11 19:20:45 +0300

    created migrations + models for quiz feature foundation

---
+++ backend/app/Models/QuestionOption.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable([
+    'text_en',
+    'text_ar',
+    'weights',
+])]
+
+class QuestionOption extends Model
+{
+    protected function casts()
+    {
+        return [
+            'weights' => 'array',
+        ];
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+}
